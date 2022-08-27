ee67b4823211a1d39f0cf0681245d0dd12f0ff30
Mathias Wadjo <55707500+wombassorville@users.noreply.github.com>
2022-08-27 20:14:14 +0000

    fix inpire service provider

--- src/Providers/InspirationServiceProvider.php
+++ src/Providers/InspirationServiceProvider.php
@@ -23,7 +23,7 @@ class InspirationServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../views', 'inspire');
     }
 }
